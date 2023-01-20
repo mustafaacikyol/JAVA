7af4b40fc20b908df328b837e0f80f265b4275ce
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2023-01-20 11:58:23 +0300

    Create Answer.java

---
+++ Exercise - 6/Answer.java
@@ -0,0 +1,115 @@
+import java.util.Scanner;
+
+abstract class Fruit {
+
+    double kilogramPrice;
+    double quantity;
+
+    abstract void fruitName();
+
+    double price() {
+        return kilogramPrice * quantity;
+    }
+
+}
+
+class Strawberry extends Fruit {
+    @Override
+    void fruitName() {
+        System.out.print("Strawberry");
+    }
+
+}
+
+class Pear extends Fruit {
+    @Override
+    void fruitName() {
+        System.out.print("Pear");
+    }
+}
+
+class Apple extends Fruit {
+    @Override
+    void fruitName() {
+        System.out.print("Apple");
+    }
+}
+
+class Watermelon extends Fruit {
+    @Override
+    void fruitName() {
+        System.out.print("Watermelon");
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        double totalPrice = 0.0;
+        Strawberry strawberry = new Strawberry();
+        Pear pear = new Pear();
+        Apple apple = new Apple();
+        Watermelon watermelon = new Watermelon();
+
+        while (true) {
+
+            System.out.println("0 : Exit");
+            System.out.println("1 : Strawberry");
+            System.out.println("2 : Pear");
+            System.out.println("3 : Apple");
+            System.out.println("4 : Watermelon");
+            System.out.print("Select the fruit you want to buy : ");
+
+            Scanner keyboard = new Scanner(System.in);
+            int choice = keyboard.nextInt();
+
+            switch (choice) {
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                    System.out.print("Enter quantity: ");
+                    strawberry.quantity = keyboard.nextDouble();
+                    System.out.print("Enter price: ");
+                    strawberry.kilogramPrice = keyboard.nextDouble();
+                    strawberry.fruitName();
+                    System.out.println(" price: " + strawberry.price());
+                    totalPrice += strawberry.price();
+                    System.out.println("Total price: " + totalPrice);
+                    break;
+                case 2:
+                    System.out.print("Enter quantity: ");
+                    pear.quantity = keyboard.nextDouble();
+                    System.out.print("Enter price: ");
+                    pear.kilogramPrice = keyboard.nextDouble();
+                    pear.fruitName();
+                    System.out.println(" price: " + pear.price());
+                    totalPrice += pear.price();
+                    System.out.println("Total price: " + totalPrice);
+                    break;
+                case 3:
+                    System.out.print("Enter quantity: ");
+                    apple.quantity = keyboard.nextDouble();
+                    System.out.print("Enter price: ");
+                    apple.kilogramPrice = keyboard.nextDouble();
+                    apple.fruitName();
+                    System.out.println(" price: " + apple.price());
+                    totalPrice += apple.price();
+                    System.out.println("Total price: " + totalPrice);
+                case 4:
+                    System.out.print("Enter quantity: ");
+                    watermelon.quantity = keyboard.nextDouble();
+                    System.out.print("Enter price: ");
+                    watermelon.kilogramPrice = keyboard.nextDouble();
+                    watermelon.fruitName();
+                    System.out.println(" price: " + watermelon.price());
+                    totalPrice += watermelon.price();
+                    System.out.println("Total price: " + totalPrice);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
